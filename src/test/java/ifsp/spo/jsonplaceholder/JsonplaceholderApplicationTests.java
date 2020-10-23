7bdf10756f437383fd4a2a036fd0aa9bae747cf6
gaugsouza <guilhermeguis@outlook.com>
2020-10-23 20:55:50 -0300

    init

---
+++ src/test/java/ifsp/spo/jsonplaceholder/JsonplaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsp.spo.jsonplaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
